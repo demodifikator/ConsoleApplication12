47a233e929114effb8ab8905abf68ee11d0959fb
Lenovo <Lenovo@DESKTOP-80UM70A>
2017-12-16 02:21:59 +0200

    Обратный порядок

--- ConsoleApplication12/Program.cs
+++ ConsoleApplication12/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApplication12
     {
         static void Main(string[] args)
         {
+            byte m, i;
+            Random rand = new Random();
+            Console.Write("Введите количество элементов массива: ");
+            m = byte.Parse(Console.ReadLine());
+            int[] arr = new int[m];
+            for (i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(0,100);
+            Console.WriteLine("В прямом порядке: ");
+            for(i = 0; i<arr.Length;i++)
+                Console.WriteLine($"{i+1}) {arr[i]}");
+
+            Console.WriteLine("В обратном порядке: ");
+            for(i = (byte)(arr.Length-1); i>=0; i--)
+                Console.WriteLine($"{i+1}) {arr[i]}");
         }
     }
 }
